18cfa05695805ddd4d3f797c523d9a0570da704b
hhhkdev <112482426+hhhkdev@users.noreply.github.com>
2024-01-12 23:43:11 +0900

    chore: 페이지 파일 생성 (#8)
    * etc: 컴포넌트 및 css 파일 생성
    
    * feat: Button 컴포넌트 초안
    
    * feat: Button 컴포넌트 테스트
    
    * chore: eslint 에러 해결
    
    * bug: eslint 오류 해결
    
    * feat: Button 관련 컴포넌트 스타일
    
    * feat: Input 태그 스타일
    
    * feat: SearchItem 컴포넌트 스타일
    
    * etc: svg 파일 추가
    
    * refac: Input 태그 수정
    
    * feat: SearchNickname 컴포넌트 스타일
    
    * refac: button 태그 설정
    
    * feat: Setting 컴포넌트 스타일
    
    * feat: Envelope 컴포넌트 스타일
    
    * chore: yarn install
    
    * refac: App.js Router화
    
    * feat: MyPage 구현
    
    * feat: 페이지 파일 생성
    
    * chore: conflicts 원인 제거

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import './App.css';
-import './components/Header.css';
+import "./App.css";
+import "./components/Header.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import fog from './assets/fog.svg';
-import snowbg1 from './assets/snow-bg-1.svg';
-import snowbg2 from './assets/snow-bg-2.svg';
-import snowbg3 from './assets/snow-bg-3.svg';
-import SnowFloor from './components/SnowFloor';
-
+import MyPage from "./pages/MyPage";
+import SettingPage from "./pages/SettingPages/SearchPage";
+import SearchPage from "./pages/SettingPages/SearchPage";
+import ChangeSnowmanPage from "./pages/SettingPages/ChangeSnowmanPage";
+import ChangeNamePage from "./pages/SettingPages/ChangeNamePage";
 
 function App() {
   return (
-    <div className='App-bg-non'>
-      {/* 배경색상 + 안개 + 하늘 눈 + 바닥 눈 ++ 페이지 내용*/}
-      <div className='App-bg'>
-        <div className='overlap-container'>
-          <img src={fog} alt="Fog Image" className='App-bg-fog' />
-          
-          {/* 하늘 눈(움직임) */}
-          <div className='snow-bg-container'>
-            <img src={snowbg1} alt="Snow Background 1" className='App-bg-snow' />
-          </div>
-
-          {/* 바닥 눈*/}
-          <SnowFloor/>
-
-          {/* 이 아래에 페이지 내용들 추가하면 됨. */}
-          <div className='main-container'>
-
-          </div>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MyPage></MyPage>}></Route>
+        <Route path="/setting" element={<SettingPage></SettingPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/IndexPages/SignupLocationPage.js
@@ -0,0 +1,21 @@
+import "./SignupLocationPage.css";
+
+import fog from "../../assets/fog.svg";
+
+const SignupLocationPage = () => {
+  return (
+    <div className="App-bg-non">
+      {/* 배경색상 + 안개 + 하늘 눈 + 바닥 눈 ++ 페이지 내용*/}
+      <div className="App-bg">
+        <div className="overlap-container">
+          <img src={fog} alt="Fog Image" className="App-bg-fog" />
+
+          {/* 이 아래에 페이지 내용들 추가하면 됨. */}
+          <div className="main-container"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupLocationPage;
