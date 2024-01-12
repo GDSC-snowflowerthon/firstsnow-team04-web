15d21cf209f9ffb055c234b6055dcb1ef4fdf9d6
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-13 01:38:01 +0900

    feat: Setting 페이지 기능 제외

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,28 +1,37 @@
-import emailIcon from "../assets/change-email.svg";
 import locationIcon from "../assets/change-location.svg";
 import nameIcon from "../assets/change-name.svg";
 import snowmanIcon from "../assets/change-snowman.svg";
 import loginIcon from "../assets/setting-login.svg";
 import logoutIcon from "../assets/setting-logout.svg";
-import mailboxIcon from "../assets/setting-mailbox.svg";
+
+import { Link } from "react-router-dom";
 
 import "./Setting.css";
 
+const changeName = async () => {};
+
+const changeLocation = () => {};
+
+const login = () => {};
+
+const logout = () => {};
+
+const changeSnowman = () => {};
+
 const SETTING = {
-  email: [emailIcon, "이메일 변경하기"],
-  name: [nameIcon, "닉네임 변경하기"],
-  location: [locationIcon, "내 위치 정보 변경하기"],
-  mailbox: [mailboxIcon, "내 우편함으로 돌아가기"],
-  login: [loginIcon, "로그인"],
-  logout: [logoutIcon, "로그아웃"],
-  snowman: [snowmanIcon, "내 눈사람 변경하기"],
+  name: [nameIcon, "닉네임 변경하기", changeName],
+  location: [locationIcon, "내 위치 정보 변경하기", changeLocation],
+  login: [loginIcon, "로그인", login],
+  logout: [logoutIcon, "로그아웃", logout],
+  snowman: [snowmanIcon, "내 눈사람 변경하기", changeSnowman],
 };
 
 const Setting = ({ setting }) => {
+  const list = SETTING[setting];
   return (
-    <div className="Setting-Container">
-      <img src={SETTING[setting][0]} className="Setting-Icon"></img>
-      <div className="Setting-Text">{SETTING[setting][1]}</div>
+    <div className="Setting-Container" onClick={list[2]}>
+      <img src={list[0]} className="Setting-Icon"></img>
+      <div className="Setting-Text">{list[1]}</div>
     </div>
   );
 };
--- src/components/SettingToggle.js
+++ src/components/SettingToggle.js
@@ -1,19 +1,24 @@
 import Switch from "@mui/material/Switch";
 import userIcon from "../assets/change-user.svg";
 import "./Setting.css";
+import { useState } from "react";
 
-const SETTING = {
-  on: "회원에게만 받기",
-  off: "누구에게나 받기",
-};
+const SETTING = ["누구에게나 받기", "회원에게만 받기"];
 
-const SettingToggle = ({ who }) => {
+const SettingToggle = ({ init }) => {
+  const [toggle, setToggle] = useState(init);
+  /* 0: off, 1: on */
   return (
     <div className="Setting-Container">
       <img src={userIcon} className="Setting-Icon"></img>
       <div className="Setting-Wrapper">
-        <div className="Setting-Text">{SETTING[who]}</div>
-        <Switch></Switch>
+        <div className="Setting-Text">{SETTING[toggle]}</div>
+        <Switch
+          checked={toggle}
+          onChange={(e) => {
+            setToggle(Number(!toggle));
+          }}
+        ></Switch>
       </div>
     </div>
   );
