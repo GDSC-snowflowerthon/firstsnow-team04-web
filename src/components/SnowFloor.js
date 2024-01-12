bde2a2cf0f2bdeab3ecd4d76843cf955e18eca99
Yarrly <74090200+eunsoA@users.noreply.github.com>
2024-01-12 19:01:14 +0900

    docs: 배경

---
+++ src/components/SnowFloor.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css';
+
+function SnowFloor(){
+    return (
+    <div className="App-bg-snow-floor-container">
+        <div className="App-bg-snow-floor-top"></div>
+        <div className="App-bg-snow-floor-middle"></div>
+        <div className="App-bg-snow-floor-bottom"></div>
+    </div>
+    );
+}
+
+export default SnowFloor;
