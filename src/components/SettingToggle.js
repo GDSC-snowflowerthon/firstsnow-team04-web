245bb5d4c3c8004997a0fe27293bc28d0d8eb29e
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-12 20:40:23 +0900

    feat: Setting 컴포넌트 스타일

--- src/components/SettingToggle.js
+++ src/components/SettingToggle.js
@@ -1,11 +1,22 @@
 import Switch from "@mui/material/Switch";
+import userIcon from "../assets/change-user.svg";
+import "./Setting.css";
 
-const SettingToggle = () => {
+const SETTING = {
+  on: "회원에게만 받기",
+  off: "누구에게나 받기",
+};
+
+const SettingToggle = ({ who }) => {
   return (
-    <div>
-      <img></img>
-      <div></div>
-      <Switch></Switch>
+    <div className="Setting-Container">
+      <img src={userIcon} className="Setting-Icon"></img>
+      <div className="Setting-Wrapper">
+        <div className="Setting-Text">{SETTING[who]}</div>
+        <Switch></Switch>
+      </div>
     </div>
   );
 };
+
+export default SettingToggle;
