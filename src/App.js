3cc091d50e3f0a231b2026a28a0bf6fdd2bd5805
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-12 22:57:20 +0900

    refac: App.js Router화

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import './App.css';
-import './components/Header.css';
+import "./App.css";
+import "./components/Header.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import fog from './assets/fog.svg';
-import snowbg1 from './assets/snow-bg-1.svg';
-import snowbg2 from './assets/snow-bg-2.svg';
-import snowbg3 from './assets/snow-bg-3.svg';
-import SnowFloor from './components/SnowFloor';
-
+import SettingPage from "./pages/SettingPage";
 
 function App() {
   return (
-    <div className='App-bg-non'>
-      {/* 배경색상 + 안개 + 하늘 눈 + 바닥 눈 ++ 페이지 내용*/}
-      <div className='App-bg'>
-        <div className='overlap-container'>
-          <img src={fog} alt="Fog Image" className='App-bg-fog' />
-          
-          {/* 하늘 눈(움직임) */}
-          <div className='snow-bg-container'>
-            <img src={snowbg1} alt="Snow Background 1" className='App-bg-snow' />
-          </div>
-
-          {/* 바닥 눈*/}
-          <SnowFloor/>
-
-          {/* 이 아래에 페이지 내용들 추가하면 됨. */}
-          <div className='main-container'>
-
-          </div>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SettingPage></SettingPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
