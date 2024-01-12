b082d9bec1b3538827645e8139aa5494938c64fe
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-12 21:12:47 +0900

    feat: Envelope 컴포넌트 스타일

--- src/components/Envelope.js
+++ src/components/Envelope.js
@@ -1,8 +1,17 @@
-const Envelope = () => {
+import closed from "../assets/envelope-close.svg";
+import opened from "../assets/envelope-open.svg";
+import "./Envelope.css";
+
+const IMG = {
+  ["open"]: opened,
+  ["close"]: closed,
+};
+
+const Envelope = ({ letter }) => {
   return (
-    <div>
-      <img></img>
-      <div></div>
+    <div className="Envelope-Container">
+      <img className="Envelope" src={IMG[letter.is_open]}></img>
+      <div className="Envelope-Text">{letter.sender_id}</div>
     </div>
   );
 };
