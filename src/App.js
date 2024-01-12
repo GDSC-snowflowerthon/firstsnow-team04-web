4b118cf4d1c48d25e657aaae9f8f8c5ffdb94e01
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-13 03:26:46 +0900

    fix: Input 수정

--- src/App.js
+++ src/App.js
@@ -3,17 +3,36 @@ import "./components/Header.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MyPage from "./pages/MyPage";
-import SettingPage from "./pages/SettingPages/SearchPage";
+import SettingPage from "./pages/SettingPages/SettingPage";
 import SearchPage from "./pages/SettingPages/SearchPage";
 import ChangeSnowmanPage from "./pages/SettingPages/ChangeSnowmanPage";
+import ChangeLoactionPage from "./pages/SettingPages/ChangeLocationPage";
 import ChangeNamePage from "./pages/SettingPages/ChangeNamePage";
+import IndexPage from "./pages/IndexPages/IndexPage";
+import LoginPage from "./pages/IndexPages/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MyPage></MyPage>}></Route>
-        <Route path="/setting" element={<SettingPage></SettingPage>}></Route>
+        <Route path="/" element={<IndexPage></IndexPage>}></Route>
+        <Route path="/main" element={<MyPage></MyPage>}></Route>
+        <Route path="/setting">
+          <Route index element={<SettingPage></SettingPage>}></Route>
+          <Route
+            path="name"
+            element={<ChangeNamePage></ChangeNamePage>}
+          ></Route>
+          <Route
+            path="location"
+            element={<ChangeLoactionPage></ChangeLoactionPage>}
+          ></Route>
+          <Route
+            path="snowman"
+            element={<ChangeSnowmanPage></ChangeSnowmanPage>}
+          ></Route>
+        </Route>
+        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
       </Routes>
     </BrowserRouter>
   );
