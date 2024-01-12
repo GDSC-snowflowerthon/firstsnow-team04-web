555ac3e2e97739360a3f521a04ebc6398246d6a7
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-12 23:31:24 +0900

    feat: 페이지 파일 생성

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import "./App.css";
 import "./components/Header.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SettingPage from "./pages/SettingPage";
+import MyPage from "./pages/MyPage";
+import SettingPage from "./pages/SettingPages/SearchPage";
+import SearchPage from "./pages/SettingPages/SearchPage";
+import ChangeSnowmanPage from "./pages/SettingPages/ChangeSnowmanPage";
+import ChangeNamePage from "./pages/SettingPages/ChangeNamePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SettingPage></SettingPage>}></Route>
+        <Route path="/" element={<MyPage></MyPage>}></Route>
+        <Route path="/setting" element={<SettingPage></SettingPage>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/MailPage.js
@@ -0,0 +1,21 @@
+import "./MailPage.css";
+
+import fog from "../assets/fog.svg";
+
+const MailPage = () => {
+  return (
+    <div className="App-bg-non">
+      {/* 배경색상 + 안개 + 하늘 눈 + 바닥 눈 ++ 페이지 내용*/}
+      <div className="App-bg">
+        <div className="overlap-container">
+          <img src={fog} alt="Fog Image" className="App-bg-fog" />
+
+          {/* 이 아래에 페이지 내용들 추가하면 됨. */}
+          <div className="main-container"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MailPage;
