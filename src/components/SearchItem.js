91bec3c6c36b4c337c7f13facbc305174e7e6635
hhhkdev <c235335@g.hongik.ac.kr>
2024-01-12 19:27:12 +0900

    feat: SearchItem 컴포넌트 스타일

--- src/components/SearchItem.js
+++ src/components/SearchItem.js
@@ -1,8 +1,10 @@
-const SearchItem = () => {
+import "./Search.css";
+
+const SearchItem = ({ name, email }) => {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="SearchItem-Container">
+      <div className="SearchItem-Name">{name}</div>
+      <div className="SearchItem-Email">{email}</div>
     </div>
   );
 };
